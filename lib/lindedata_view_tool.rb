178c6ff8f201af11a06dcfe044c54671c519cc2f
VajkFreyasman <craig.linde.com>
2018-01-31 14:49:49 -0700

    LindeData Ruby Gem

---
+++ lib/lindedata_view_tool.rb
@@ -0,0 +1,6 @@
+require "lindedata_view_tool/version"
+require "lindedata_view_tool/renderrer"
+
+module LindedataViewTool
+  # Your code goes here...
+end
---
+++ lib/lindedata_view_tool/version.rb
@@ -0,0 +1,3 @@
+module LindedataViewTool
+  VERSION = "0.1.0"
+end
